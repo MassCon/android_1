ab7a7a3dc7226688e996a9bfc610da89636749b1
MassCon <53021665+MassCon@users.noreply.github.com>
2024-04-19 19:08:36 +0200

    14.02.2024 hw

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,53 +1,111 @@
 package com.example.helloworld;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.Manifest;
-import android.content.ContentResolver;
-import android.database.Cursor;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.GridLayout;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button showNotificationButton;
+    private static final String APP_LAUNCH_COUNTER = "appLaunchCount";
+    private static final String PREFERENCES_NAME = "myPreferences";
+    private int chosenRating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showNotificationButton = findViewById(R.id.notify);
+        showRatingDialogIfNeeded();
+    }
+    private void showRatingDialogIfNeeded() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+        int launchCount = sharedPreferences.getInt(APP_LAUNCH_COUNTER, 0);
+        launchCount++;
 
-        showNotificationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Notification helper = new Notification(MainActivity.this);
-                helper.showNotification("My Notification", "This notification appeared from the button!");
-            }
+        if (launchCount % 1 == 0) {
+            showRatingDialog();
+        }
+
+        sharedPreferences.edit().putInt(APP_LAUNCH_COUNTER, launchCount).apply();
+    }
+
+    private void showRatingDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.rating_dialog, null);
+        builder.setView(dialogView);
+
+        ImageView[] stars = new ImageView[5];
+
+        stars[0] = dialogView.findViewById(R.id.star1);
+        stars[1] = dialogView.findViewById(R.id.star2);
+        stars[2] = dialogView.findViewById(R.id.star3);
+        stars[3] = dialogView.findViewById(R.id.star4);
+        stars[4] = dialogView.findViewById(R.id.star5);
+
+        stars[0].setOnClickListener(view -> {
+            chosenRating = 1;
+            updateStarsFill(stars);
         });
+        stars[1].setOnClickListener(view -> {
+            chosenRating = 2;
+            updateStarsFill(stars);
+        });
+        stars[2].setOnClickListener(view -> {
+            chosenRating = 3;
+            updateStarsFill(stars);
+        });
+        stars[3].setOnClickListener(view -> {
+            chosenRating = 4;
+            updateStarsFill(stars);
+        });
+        stars[4].setOnClickListener(view -> {
+            chosenRating = 5;
+            updateStarsFill(stars);
+        });
+
+        builder.setPositiveButton("Submit", (dialog, which) -> {
+                    int selectedRating = chosenRating;
+                    if (selectedRating <= 3) {
+                        showFeedbackForm();
+                    } else {
+                        showPlayMarketPrompt();
+                    }
+                })
+                .create()
+                .show();
+    }
+    private void updateStarsFill(ImageView[] stars) {
+        for(int i = 0; i < chosenRating; i++) {
+            stars[i].setImageResource(android.R.drawable.star_big_on);
+        }
+        for(int i = chosenRating; i < 5; i++) {
+            stars[i].setImageResource(android.R.drawable.star_big_off);
+        }
+    }
+    private void showFeedbackForm() {
+        EditText feedbackInput = new EditText(this);
+        new AlertDialog.Builder(this)
+                .setPositiveButton("Submit", (dialog, which) -> {
+                    String feedback = feedbackInput.getText().toString();
+                })
+                .create()
+                .show();
+
+    }
+
+    private void showPlayMarketPrompt() {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("Rate on Play Store", (dialog, which) -> {
+                    String appUri = "market://details?id=" + getPackageName();
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(appUri)));
+                })
+                .create()
+                .show();
     }
 }
